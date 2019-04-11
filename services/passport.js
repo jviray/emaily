8a0b93a950ab9b99756a6f0505e619cd85684112
Jonathan Viray <jfviray7@gmail.com>
2019-04-11 16:00:34 -0400

    Refactor passport Google strategy to use asycn/await

--- services/passport.js
+++ services/passport.js
@@ -23,19 +23,15 @@ passport.use(
       callbackURL: '/auth/google/callback',
       proxy: true
     },
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id }).then(existingUser => {
-        if (existingUser) {
-          // User document w/ profile.id already exists!
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
+      if (existingUser) {
+        // null below indicates that there was no error
+        return done(null, existingUser);
+      }
 
-          // null below indicates that there was no error
-          done(null, existingUser);
-        } else {
-          new User({ googleId: profile.id })
-            .save()
-            .then(user => done(null, user));
-        }
-      });
+      const user = await new User({ googleId: profile.id }).save();
+      return done(null, user);
     }
   )
 );
