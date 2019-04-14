344f37ca41a7aeee4f2c40f7d3f8b6445fd1a451
Jonathan Viray <jfviray7@gmail.com>
2019-04-14 05:08:17 -0400

    Use redux store to render header content accordingly

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,25 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
 import Header from './Header';
-const Dashboard = () => <h2>Dashboard</h2>;
-const SurveyNew = () => <h2>SurveyNew</h2>;
-const Landing = () => <h2>Landing</h2>;
+import Landing from './Landing';
+const Dashboard = () => <h4>Dashboard</h4>;
+const SurveyNew = () => <h4>SurveyNew</h4>;
 
 // Functional React component syntax
-const App = () => {
-  return (
-    <div>
-      <BrowserRouter>
-        <div>
-          <Header />
-          <Route exact path="/" component={Landing} />
-          <Route exact path="/surveys" component={Dashboard} />
-          <Route path="/surveys/new" component={SurveyNew} />
-        </div>
-      </BrowserRouter>
-    </div>
-  );
-};
+class App extends Component {
+  componentDidMount() {
+    this.props.fetchUser();
+  }
 
-export default App;
+  render() {
+    return (
+      <div className="container">
+        <BrowserRouter>
+          <Header />
+          <div style={{ paddingRight: '15px', paddingLeft: '15px' }}>
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/surveys" component={Dashboard} />
+            <Route path="/surveys/new" component={SurveyNew} />
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
+
+export default connect(
+  null,
+  actions
+)(App);
---
+++ client/src/components/Landing.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Landing = () => {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h2>Welcome to Emaily!</h2>
+      Send email surveys to your users and collect feedback.
+    </div>
+  );
+};
+
+export default Landing;
