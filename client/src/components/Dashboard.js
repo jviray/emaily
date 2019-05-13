6869f89e6be9310483b75e2508cecaeb79c4bbcd
Jonathan Viray <jfviray7@gmail.com>
2019-05-13 08:08:55 -0400

    Wire updated Redux code with React

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import SurveyList from './surveys/SurveyList';
 
 // No need for class based component (using functional component)
 const Dashboard = () => {
   return (
     <div>
-      <h4>Dashboard</h4>
+      <SurveyList />
       <div className="fixed-action-btn">
         <Link to="/surveys/new" className="btn-floating btn-large red">
           <i className="material-icons">add</i>
---
+++ client/src/components/surveys/SurveyList.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchSurveys } from '../../actions';
+
+class SurveyList extends Component {
+  componentDidMount() {
+    this.props.fetchSurveys();
+  }
+
+  render() {
+    return (
+      <div>
+        <h4>Survey List</h4>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps({ surveys }) {
+  return { surveys };
+}
+
+export default connect(
+  mapStateToProps,
+  { fetchSurveys }
+)(SurveyList);
